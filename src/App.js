29149f13efef876b4041d7cc8613bd9ab598416c
Ay31 <525998520@qq.com>
2020-07-27 21:10:14 +0800

    feat(app.js): 事件处理

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+    {
+        title: 'React',
+        url: 'https://facebook.github.io/react/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://github.com/reactjs/redux',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+]
+const helloWorld = 'welcome to React'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            list,
+        }
+        this.onDismiss = this.onDismiss.bind(this)
+    }
+    onDismiss(id) {
+        const updatedList = this.state.list.filter(
+            (item) => item.objectID !== id
+        )
+        this.setState({
+            list: updatedList,
+        })
+    }
+    render() {
+        return (
+            <div className="App">
+                <form>
+                    <input type="text" onChange={this.onSearchChange}></input>
+                </form>
+                {/* <h2> */}
+                {this.state.list.map((item) => {
+                    return (
+                        <div key={item.objectID}>
+                            <span>
+                                <a href={item.url}>{item.title}</a>
+                            </span>
+                            <span>{item.author}</span>
+                            <span>{item.num_comments}</span>
+                            <span>{item.points}</span>
+                            <span>
+                                <button
+                                    onClick={() =>
+                                        this.onDismiss(item.objectID)
+                                    }
+                                    type="button"
+                                >
+                                    Dismiss
+                                </button>
+                            </span>
+                        </div>
+                    )
+                })}
+                {/* </h2> */}
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
